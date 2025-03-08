306d5bb7bf5558d9fbab7947eb9bafc1d1525bc3
Avi Sinha <avi15sinha@gmail.com>
2025-03-08 12:20:48 +0530

    match centre page

---
+++ frontend-avi/src/components/Matches/MatchCentre.js
@@ -0,0 +1,9 @@
+import "./MatchCentre.css";
+
+export default function MatchCentre() {
+  return (
+    <div className="match-centre-container">
+      <div className="match-centre-card">Match Centre</div>
+    </div>
+  );
+}
